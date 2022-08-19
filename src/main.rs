5d0e43e2f1417d31a297675ab30aacd597819cce
Ellen Marie Dash <me@duckie.co>
2022-08-19 17:34:39 -0400

    Add "--version" flag.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@ fn main() -> Result<(), std::io::Error> {
     let mut stack: Vec<i128> = Vec::new();
 
     let args: Vec<_> = env::args().collect();
-    if args.len() > 1 && (args[1] == "-h" || args[1] == "--help") {
-        println!("Usage, interactive: chewy");
-        println!("   non-interactive: echo \"1 2 3 + *\" | chewy");
-        println!("");
-        execute(&mut stack, "h q");
+    if let Some(flag) = args.get(1) {
+        if flag == "--version" {
+            execute(&mut stack, "V q");
+        } else {
+            println!("Usage, interactive: chewy");
+            println!("   non-interactive: echo \"1 2 3 + *\" | chewy");
+            println!("");
+            execute(&mut stack, "h q");
+        }
     }
-
 
     let stdin = io::stdin();
 
